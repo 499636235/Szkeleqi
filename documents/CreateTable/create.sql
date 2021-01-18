1dd789c4ebd33d659ee279a9c886a1a7bbc61824
Nick.Y <499636235@qq.com>
2021-01-19 00:08:02 +0800

    建表语句

---
+++ documents/CreateTable/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `picture_list_index` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `listid` int(11) NOT NULL COMMENT '图片列表id',
+  `listname` varchar(255) COMMENT '图片列表名',
+  `listindex` int(11) NOT NULL COMMENT '图片列表下标',
+  `productid` int(11) DEFAULT NULL COMMENT '产品id',
+  `createdate` datetime NOT NULL COMMENT '数据创建日期',
+  `updatedate` datetime NOT NULL COMMENT '数据更新日期',
+  PRIMARY KEY (`id`,`listid`,`listindex`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `product` (
+  `productid` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增 产品id',
+  `productclass1` varchar(255) DEFAULT NULL COMMENT '一级分类',
+  `productclass2` varchar(255) DEFAULT NULL COMMENT '二级分类',
+  `productclass3` varchar(255) DEFAULT NULL COMMENT '三级分类',
+  `productname` varchar(255) NOT NULL COMMENT '产品名称',
+  `picturename` varchar(255) DEFAULT NULL COMMENT '产品图片名',
+  `picturepath` varchar(255) DEFAULT NULL COMMENT '产品图片路径',
+  `createdate` datetime NOT NULL COMMENT '数据创建日期',
+  `updatedate` datetime NOT NULL COMMENT '数据更新日期',
+  PRIMARY KEY (`productid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  `passWord` varchar(255) NOT NULL,
+  `userGroup` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
